0f983651eee9aeaa5dac31d791fb61c5bb15dd8f
ChiquiAraujo <ck2inf@gmail.com>
2023-11-04 18:29:16 +0100

    add Mailing

---
+++ src/services/service.js
@@ -0,0 +1,29 @@
+import { Express } from 'express';
+
+const sgMail = require('@sendgrid/mail');
+const apiKey = process.env.SENDGRID_API_KEY;
+sgMail.setApiKey(apiKey);
+
+const sendPurchaseConfirmationEmail = async (to, items) => {
+  const subject = 'Confirmación de compra';
+  const text = `Tu compra ha sido confirmada. Detalles: ${items.map(item => item.name).join(', ')}`;
+  const html = `<strong>${text}</strong>`;
+
+  const msg = {
+    to, 
+    from: process.env.EMAIL_FROM, 
+    subject,
+    text,
+    html,
+  };
+
+  try {
+    await sgMail.send(msg);
+    console.log('Email enviado correctamente.');
+  } catch (error) {
+    console.error('Error enviando email de confirmación de compra:', error);
+    throw new Error('Error al enviar el email de confirmación de compra.');
+  }
+};
+
+module.exports = { sendPurchaseConfirmationEmail };
